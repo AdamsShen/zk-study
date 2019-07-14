938b52b8029878e6003320bb15e640922c017fd9
shensi <si.shen.aihuishou.com>
2019-07-14 23:46:44 +0800

    zk study

---
+++ src/main/java/edu/dongnao/zk/OrderCodeGeneratorSingle.java
@@ -0,0 +1,16 @@
+package edu.dongnao.zk;
+
+/**
+ * 订单编号生成类
+ * OrderCodeGenerator
+ */
+public class OrderCodeGeneratorSingle {
+	
+	static class InstanceHolder {
+		private static OrderCodeGenerator instance = new OrderCodeGenerator();
+	}
+	
+	public static OrderCodeGenerator getInstance() {
+		return InstanceHolder.instance;
+	}
+}
